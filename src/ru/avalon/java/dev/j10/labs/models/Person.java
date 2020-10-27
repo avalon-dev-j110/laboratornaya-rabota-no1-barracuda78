cb28c1e7be7288de10e907c71fd160119bed9956
Andrey Ruzaev <barracuda78@yandex.ru>
2020-10-28 00:07:08 +0300

    fixing duplicate fields in Person.java

--- src/ru/avalon/java/dev/j10/labs/models/Person.java
+++ src/ru/avalon/java/dev/j10/labs/models/Person.java
@@ -13,23 +13,12 @@ import ru.avalon.java.dev.j10.labs.commons.Address;
  * </ol>
  */
 public class Person {
-    private String lastName;
-    private String firstName;
-    private String secondFirstName;
-    private String middleName;
     private Passport passport;
     private Address address;
 
     public Person(Passport passport, Address address){
            this.passport = passport;
            this.address = address;
-           
-           lastName = passport.getLastName();
-           firstName = passport.getFirstName();
-           secondFirstName = passport.getSecondFirstName();
-           
-           if(passport.getMiddleName() != null)
-                middleName = passport.getMiddleName();
     }
     
     
@@ -57,22 +46,22 @@ public class Person {
         
         //     * Если у человека есть Имя, Фамилия и Отчество, то
         //     * возвращет Имя, Фимилию и Отчество, разделённые пробелом.
-        if(lastName != null && firstName != null  && middleName != null ){ //|| !middleName.equals("")
-            return String.join(" ", firstName, lastName, middleName);
+        if(passport.getLastName() != null && passport.getFirstName() != null  && passport.getMiddleName() != null ){ //|| !middleName.equals("")
+            return String.join(" ", passport.getFirstName(), passport.getLastName(), passport.getMiddleName());
             
         //     * Если у человека нет Отчества, но есть второе имя, то
         //     * возвращает Имя, Первую букву второго имени, и Фамилию,
         //     * разделённые пробелом. После Инициала второго имени
         //     * должна стоять точка. Например, "Джером К. Джером".
-        }else if(middleName == null && secondFirstName != null){
+        }else if(passport.getMiddleName() == null && passport.getSecondFirstName() != null){
             StringBuilder sb = new StringBuilder();
-            return sb.append(firstName).append(" ").append(secondFirstName.substring(0, 1)).append(". ").append(lastName).toString();
+            return sb.append(passport.getFirstName()).append(" ").append(passport.getSecondFirstName().substring(0, 1)).append(". ").append(passport.getLastName()).toString();
 
         //* Если у человека нет ни Отчества ни Второго имени, а
         //* есть только Имя и Фамилия, то возвращает их, разделённые
         //* пробелом.    
-        }else if(middleName == null || middleName.equals("") && secondFirstName == null || secondFirstName.equals("") && firstName != null || !firstName.equals("") && lastName != null || !lastName.equals("")){
-            return firstName + " " + lastName;
+        }else if(passport.getMiddleName() == null || passport.getMiddleName().equals("") && passport.getSecondFirstName() == null || passport.getSecondFirstName().equals("") && passport.getFirstName() != null || !passport.getFirstName().equals("") && passport.getLastName() != null || !passport.getLastName().equals("")){
+            return passport.getFirstName() + " " + passport.getLastName();
         }
         
         return null;
